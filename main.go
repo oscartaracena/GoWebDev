f57f8af3db9c5d1816a021925f32ca61b1de4e07
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-04 11:27:26 -0700

    using Gorilla MUX for routing, added anf FAQ page

--- main.go
+++ main.go
@@ -17,9 +17,14 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send me to <a href=\"mailto:oscar.taracena@gmail.com\">oscar.taracena@gmail.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ - Coming soon!!!</h1>")
+}
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
