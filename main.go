4f96f6fdc61bbc962e572c9d15c61b9eadf8721c
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-04 09:07:05 -0700

    added some if/else logic and home page and contact page

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	//fmt.Fprintf(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send me to <a href=\"mailto:oscar.taracena@gmail.com\">oscar.taracena@gmail.com</a>.")
+	}
+
 }
 
 func main() {
