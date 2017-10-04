09042c15b9da6dd487912abf7dedf0a64770423c
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-04 11:12:51 -0700

    using Gorilla MUX for routing, changed code to use not using if/else logic

--- main.go
+++ main.go
@@ -7,23 +7,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	//fmt.Fprintf(w, r.URL.Path)
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send me to <a href=\"mailto:oscar.taracena@gmail.com\">oscar.taracena@gmail.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1> We could not find the page you are looking for :(</h1><p>please email us if you keep getting sent to an invalid page</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send me to <a href=\"mailto:oscar.taracena@gmail.com\">oscar.taracena@gmail.com</a>.")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlerFunc)
-	r.HandleFunc("/contact", handlerFunc)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", r)
 }
