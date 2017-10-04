613148ce8d7d28efc4a607c73ceb9488c7205c04
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-04 09:20:35 -0700

    added some if/else logic and also 404 page

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send me to <a href=\"mailto:oscar.taracena@gmail.com\">oscar.taracena@gmail.com</a>.")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1> We could not find the page you are looking for :(</h1><p>please email us if you keep getting sent to an invalid page</p>")
 	}
 
