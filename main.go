83585c4e19baf5f59dc34d05424df9e8a4336391
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-04 11:39:15 -0700

    using Gorilla MUX for routing, added custom 404 page

--- main.go
+++ main.go
@@ -21,8 +21,16 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>FAQ - Coming soon!!!</h1>")
 }
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, We could not find the page you were looking for!!!</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
