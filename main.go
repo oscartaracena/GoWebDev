e1ed443003f6138e38098e946cf0a72eac581880
taracena <oscar.taracena@jpl.nasa.gov>
2017-10-05 09:41:57 -0700

    added footer/template

--- main.go
+++ main.go
@@ -40,7 +40,10 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var err error
-	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	homeTemplate, err = template.ParseFiles(
+		"views/home.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 	if err != nil {
 		panic(err)
 	}
